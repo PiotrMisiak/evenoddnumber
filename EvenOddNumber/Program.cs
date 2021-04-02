d8e9f1e0696e4e26d99bd57f00e847dc94558581
PiotrMisiak <piotrektrumpet@gmail.com>
2021-04-02 15:36:07 +0200

    Add project files.

---
+++ EvenOddNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikacja sprawdza czy dana liczba jest parzysta czy nieparzysta.\n\nPodaj liczbę całkowitą:");
+            var userNumber = GetNumber();
+            
+            if (userNumber % 2 == 0)
+            {
+                Console.WriteLine("\nPodana liczba jest parzysta.");
+            }
+            else 
+                Console.WriteLine("\nPodana liczba jest nieparzysta.");
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("\nNie wprowadziłeś poprawnej liczby, spróbuj ponownie.");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
